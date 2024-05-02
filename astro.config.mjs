4780da0d946831eaede603c198b19df3f322bfe5
Joshua Michaels <joshuaiz@me.com>
2024-05-01 21:45:10 -0500

    adds tailwind + vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind({
+    applyBaseStyles: false,
+    nesting: true
+  })],
+  output: "server",
+  adapter: vercel()
+});
