1c9eb63eeb60e93e8ad6eb332c285b403e9b81c9
Joshua Michaels <joshuaiz@me.com>
2024-07-11 20:21:38 -0500

    updates SDK + form; fixes multiple uploads

--- src/components/Upload/ImageUploader.jsx
+++ src/components/Upload/ImageUploader.jsx
@@ -5,6 +5,8 @@ import { UploadButton } from '@utils/uploadthing.ts'
 
 export function ImageUploader({ submissionData }) {
     const [uploading, setUploading] = useState(false)
+    const [uploadReady, setUploadReady] = useState(false)
+    const [files, setFiles] = useState([])
     const [uploadComplete, setUploadComplete] = useState(false)
 
     console.log('submissionData in ImageUploader', submissionData)
@@ -20,6 +22,7 @@ export function ImageUploader({ submissionData }) {
                     // Do something with the response
                     console.log('Files: ', res)
                     setUploading(false)
+                    setUploadReady(false)
 
                     localStorage.removeItem('submissionData')
                     setUploadComplete(true)
@@ -38,6 +41,11 @@ export function ImageUploader({ submissionData }) {
                     if (!formId || formId === '' || formId === 'undefined') {
                         formId = 'default'
                     }
+                    console.log('files on ready', files)
+                    if (files && files.length > 0) {
+                        setUploadReady(true)
+                        setFiles(files)
+                    }
                     return files.map(
                         (f) =>
                             new File([f], formId + '-' + f.name, {
@@ -48,6 +56,7 @@ export function ImageUploader({ submissionData }) {
                 onUploadBegin={(name) => {
                     // Do something once upload begins
                     setUploading(true)
+
                     console.log('Uploading: ', name)
                 }}
             />
@@ -60,13 +69,29 @@ export function ImageUploader({ submissionData }) {
                 </p>
             </div>
 
+            {uploadReady && (
+                <div className="flex justify-center flex flex-col">
+                    <h3 className="mb-3">Your Files:</h3>
+                    <ul>
+                        {files.map((file, index) => (
+                            <li className="text-sm" key={index}>
+                                {file.name}
+                            </li>
+                        ))}
+                    </ul>
+                    <p>
+                        Please click the Upload button above to start uploading.
+                    </p>
+                </div>
+            )}
+
             {uploading && (
                 <div className="w-full h-full bg-black bg-opacity-50 flex items-center justify-center p-12">
-                    <div className="bg-white p-4 rounded-md max-w-[400px]">
+                    <div className="bg-white p-4 rounded-md max-w-[300px] lg:max-w-[500px]">
                         <p>
                             Uploading... Please do not leave this page or use
                             the back button until uploading is complete. This
-                            may take a few minutes, epscially for large files.
+                            may take a few minutes, especially for large files.
                             Please be patient. Thank you!
                         </p>
                     </div>
